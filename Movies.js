1b9967c61ecaa3ed86b30c5fb2f6ba0142296d6d
I-Code777 <191649398+I-Code777@users.noreply.github.com>
2025-04-06 20:02:56 -0600

    edits
    made edits as required for steps 1 through 2

--- Movies.js
+++ Movies.js
@@ -4,8 +4,20 @@ var Schema = mongoose.Schema;
 mongoose.connect(process.env.DB);
 
 // Movie schema
+// this section was taken from the last assignment
 var MovieSchema = new Schema({
-
+    title: { type: String, required: true, index: true },
+    releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100']},
+    genre: {
+      type: String,
+      enum: [
+        'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
+      ],
+    },
+    actors: [{
+      actorName: String,
+      characterName: String,
+    }],
 });
 
 // return the model
--- Reviews.js
+++ Reviews.js
@@ -5,7 +5,10 @@ mongoose.connect(process.env.DB);
 
 // Movie schema
 var ReviewSchema = new Schema({
-
+    movieId: { type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }, //referst to Movie collection
+    username: String, //name of reviewer
+    review: String, //review body text
+    rating: { type: Number, min: 0, max: 5 }
 });
 
 // return the model
