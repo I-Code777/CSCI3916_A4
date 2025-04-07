d24cf86d7c1f6a67a43f5d5c182154ee63bceecf
I-Code777 <191649398+I-Code777@users.noreply.github.com>
2025-04-06 22:36:14 -0600

    Update server.js
    worked on extra credit

--- server.js
+++ server.js
@@ -15,7 +15,19 @@ var User = require('./Users');
 var Movie = require('./Movies');
 var Review = require('./Reviews');
 var mongoose = require('mongoose');
-const GA_TRACKING_ID = process.env.GA_KEY; // for the extra credit
+//extra credit section=================================
+var express = require('express');
+var bodyParser = require('body-parser');
+const crypto = require("crypto");
+var rp = require('request-promise');
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var router = express.Router();
+
+const GA_TRACKING_ID = process.env.GA_KEY;
+//====================================================
 
 var app = express();
 app.use(cors());
@@ -58,6 +70,52 @@ function getJSONObjectForMovieRequirement(req) {
 
     return json;
 }
+
+//extra Credit section========================================================================================================================
+function trackDimension(category, action, label, value, dimension, metric) {
+
+    var options = { method: 'GET',
+        url: 'https://www.google-analytics.com/collect',
+        qs:
+            {   // API Version.
+                v: '1',
+                // Tracking ID / Property ID.
+                tid: GA_TRACKING_ID,
+                // Random Client Identifier. Ideally, this should be a UUID that
+                // is associated with particular user, device, or browser instance.
+                cid: crypto.randomBytes(16).toString("hex"),
+                // Event hit type.
+                t: 'event',
+                // Event category.
+                ec: category,
+                // Event action.
+                ea: action,
+                // Event label.
+                el: label,
+                // Event value.
+                ev: value,
+                // Custom Dimension
+                cd1: dimension,
+                // Custom Metric
+                cm1: metric
+            },
+        headers:
+            {  'Cache-Control': 'no-cache' } };
+
+    return rp(options);
+}
+
+router.route('/test')
+    .get(function (req, res) {
+        // Event value must be numeric.
+        trackDimension('Feedback', 'Rating', 'Feedback for Movie', '3', 'Guardian\'s of the Galaxy 2', '1')
+            .then(function (response) {
+                console.log(response.body);
+                res.status(200).send('Event tracked.').end();
+            })
+    });
+
+//============================================================================================================================================
 
 router.post('/signup', function(req, res) {
     if (!req.body.username || !req.body.password) {
